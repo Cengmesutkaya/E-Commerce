f5cb1571066e844e4f43bcf8f604cd96cda6fe76
Mesut KAYA <mesutkaya@anadolu.edu.tr>
2020-12-30 15:41:04 +0300

    Last changes in 30.12.2020

---
+++ E-Commerce/E-Commerce/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
